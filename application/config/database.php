f1ef9288af9c2ab0e32a1977f4f55c38373855bd
sahil khandelwal <skhandelwal758.sk@gmail.com>
2021-07-20 11:34:10 +0530

    set base url dynamically in config.php and in database.php set hostname dynamically

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => $_SERVER['SERVER_NAME'],
 	'username' => 'root',
 	'password' => '',
 	'database' => 'login',
